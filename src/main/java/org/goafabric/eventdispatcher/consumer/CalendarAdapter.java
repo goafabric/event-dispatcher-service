6d37f9a7b2e812ce2861b8913b60f341c3ea1253
Andreas Mautsch <amautsch>
2022-04-23 11:19:16 +0200

    queue changes

--- src/main/java/org/goafabric/eventdispatcher/consumer/CalendarAdapter.java
+++ src/main/java/org/goafabric/eventdispatcher/consumer/CalendarAdapter.java
@@ -6,6 +6,8 @@ import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import static org.goafabric.eventdispatcher.listener.ListenerConstants.*;
@@ -17,8 +19,8 @@ public class CalendarAdapter {
 
     @RabbitListener(bindings = @QueueBinding(value = @Queue(name = QUEUE_NAME),
             exchange = @Exchange(value = PATIENT, type = ExchangeTypes.TOPIC), key = {CREATE}))
-    public void createPatient(String id) {
-        log.info("patient create for id = {}", id);
+    public void createPatient(@Header(AmqpHeaders.RECEIVED_EXCHANGE) String exchange, String id) {
+        log.info("patient create for id = {}; exchange {}", id, exchange);
     }
 
     @RabbitListener(bindings = @QueueBinding(value = @Queue(name = QUEUE_NAME),
@@ -42,12 +44,18 @@ public class CalendarAdapter {
     @RabbitListener(bindings = @QueueBinding(value = @Queue(name = QUEUE_NAME),
             exchange = @Exchange(value = PRACTITIONER, type = ExchangeTypes.TOPIC), key = {UPDATE}))
     public void updatePractitioner(String id) {
-        log.info("practitioner create for id = {}", id);
+        log.info("practitioner update for id = {}", id);
     }
 
     @RabbitListener(bindings = @QueueBinding(value = @Queue(name = QUEUE_NAME),
             exchange = @Exchange(value = PRACTITIONER, type = ExchangeTypes.TOPIC), key = {DELETE}))
     public void deletePractitioner(String id) {
-        log.info("practitioner create for id = {}", id);
+        log.info("practitioner delete for id = {}", id);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(name = QUEUE_NAME),
+            exchange = @Exchange(value = ORGANIZATION, type = ExchangeTypes.TOPIC), key = {CREATE}))
+    public void createOrganization(String id) {
+        log.info("organization create for id = {}", id);
     }
 }
--- src/main/java/org/goafabric/eventdispatcher/producer/EventProducer.java
+++ src/main/java/org/goafabric/eventdispatcher/producer/EventProducer.java
@@ -1,5 +1,6 @@
 package org.goafabric.eventdispatcher.producer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.goafabric.eventdispatcher.service.service.ChangeEvent;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 
 @Component
+@Slf4j
 public class EventProducer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
@@ -16,6 +18,7 @@ public class EventProducer {
     private HashMap<String, String> exchanges;
 
     public void produce(ChangeEvent changeEvent) {
+        //log.info(changeEvent.toString());
         final String exchange = exchanges.get(changeEvent.getType());
         if (exchange != null) {
             rabbitTemplate.convertAndSend(
