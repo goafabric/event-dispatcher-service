f0d24b75f6ce78f5923531071d8c97535350d953
Andreas Mautsch <amautsch>
2022-04-24 09:53:52 +0200

    queue changes

--- src/main/java/org/goafabric/eventdispatcher/listener/MyListener.java
+++ src/main/java/org/goafabric/eventdispatcher/listener/MyListener.java
@@ -12,12 +12,18 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static org.goafabric.eventdispatcher.listener.ListenerConstants.CREATE;
-import static org.goafabric.eventdispatcher.listener.ListenerConstants.PATIENT;
 
 @Target({ ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @RabbitListener(bindings = @QueueBinding(value = @Queue(name = "MyQueue"),
-        exchange = @Exchange(value = PATIENT, type = ExchangeTypes.TOPIC), key = {CREATE}))
+        exchange = @Exchange(value = "patient", type = ExchangeTypes.TOPIC), key = {CREATE}))
 public @interface MyListener {
+    /*
+    @AliasFor(
+            annotation = Exchange.class,
+            attribute = "value"
+    )
+    String exchangeName() default "";
 
+     */
 }
