ce56df8b9777572a4a76ae7577058c77e8d8f6bf
Andreas Mautsch <amautsch>
2022-12-29 12:28:55 +0100

    websocket added

--- src/main/java/org/goafabric/eventdispatcher/service/controller/dto/SocketMessage.java
+++ src/main/java/org/goafabric/eventdispatcher/service/controller/dto/SocketMessage.java
@@ -1,11 +1,13 @@
 package org.goafabric.eventdispatcher.service.controller.dto;
 
 import lombok.*;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RegisterReflectionForBinding(SocketMessage.class)
 public class SocketMessage {
     private String message;
 }
