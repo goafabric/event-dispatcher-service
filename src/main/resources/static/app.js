5ddb3a2e0885fbb678933a65ddea612b9ee5d221
Andreas Mautsch <amautsch>
2022-12-29 10:24:21 +0100

    websocket added

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,31 +1,40 @@
 var stompClient = null;
 
 
-function connect() {
+function connectSocket() {
     var socket = new SockJS('/websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        setConnected(true);
+        //setConnected(true);
         console.log('Connected: ' + frame);
+        /*
         stompClient.subscribe('/topic/chat/public', function (chatMessage) {
             showGreeting(JSON.parse(chatMessage.body).content);
         });
+        */
     });
 }
 
-function disconnect() {
+function disconnectSocket() {
     if (stompClient !== null) {
         stompClient.disconnect();
     }
-    setConnected(false);
     console.log("Disconnected");
 }
 
-
-function sendName() {
-    connect();
-    stompClient.send("/app/chat/dispatcher", {}, JSON.stringify({'name': $("#name").val()}));
-    disconnect();
+function createPatient() {
+    stompClient.send("/app/createpatient", {}, "");
 }
 
+function updatePatient() {
+    stompClient.send("/app/updatepatient", {}, "");
+}
+
+function createPractitioner() {
+    stompClient.send("/app/createpractitioner", {}, "");
+}
+
+function updatePractitioner() {
+    stompClient.send("/app/updatepractitioner", {}, "");
+}
 
