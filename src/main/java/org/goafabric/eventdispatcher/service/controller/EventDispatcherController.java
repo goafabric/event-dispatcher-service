5a0314960e0b6043d4e354ed40a293a5cbd990c7
Andreas Mautsch <amautsch>
2022-12-31 17:50:04 +0100

    chamges

--- src/main/java/org/goafabric/eventdispatcher/service/controller/EventDispatcherController.java
+++ src/main/java/org/goafabric/eventdispatcher/service/controller/EventDispatcherController.java
@@ -1,12 +1,12 @@
 package org.goafabric.eventdispatcher.service.controller;
 
+import org.goafabric.eventdispatcher.service.controller.dto.ChangeEvent;
 import org.goafabric.eventdispatcher.service.controller.dto.SocketMessage;
 import org.goafabric.eventdispatcher.service.logic.EventDispatcherLogic;
-import org.goafabric.eventdispatcher.service.controller.dto.ChangeEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,9 +15,6 @@ import org.springframework.web.bind.annotation.*;
 public class EventDispatcherController {
     @Autowired
     EventDispatcherLogic eventDispatcherLogic;
-
-    @Autowired
-    private SimpMessageSendingOperations messagingTemplate;
 
     //dispatch event to be called from external rest clients
     @PostMapping(value = "dispatch", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -29,34 +26,34 @@ public class EventDispatcherController {
     //events to be called from html page
     @GetMapping("createpatient")
     @MessageMapping("createpatient")
-    public String createPatient() {
+    @SendTo("/public")
+    public SocketMessage createPatient() {
         final String message = eventDispatcherLogic.createPatient();
-        messagingTemplate.convertAndSend("/public", SocketMessage.builder().message(message).build());
-        return message;
+        return SocketMessage.builder().message(message).build();
     }
 
     @GetMapping("updatepatient")
     @MessageMapping("updatepatient")
-    public String updatePatient() {
+    @SendTo("/public")
+    public SocketMessage updatePatient() {
         final String message = eventDispatcherLogic.updatePatient();
-        messagingTemplate.convertAndSend("/public", SocketMessage.builder().message(message).build());
-        return message;
+        return SocketMessage.builder().message(message).build();
     }
 
     @GetMapping("createpractitioner")
     @MessageMapping("createpractitioner")
-    public String createPractitioner() {
+    @SendTo("/public")
+    public SocketMessage createPractitioner() {
         final String message = eventDispatcherLogic.createPractitioner();
-        messagingTemplate.convertAndSend("/public", SocketMessage.builder().message(message).build());
-        return message;
+        return SocketMessage.builder().message(message).build();
     }
 
     @GetMapping("updatepractitioner")
     @MessageMapping("updatepractitioner")
-    public String updatePractitioner() {
+    @SendTo("/public")
+    public SocketMessage updatePractitioner() {
         final String message = eventDispatcherLogic.updatePractitioner();
-        messagingTemplate.convertAndSend("/public", SocketMessage.builder().message(message).build());
-        return message;
+        return SocketMessage.builder().message(message).build();
     }
 }
 
