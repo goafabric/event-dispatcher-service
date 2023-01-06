41ae5c31215c07153fa97dc4877f542bab40f5d0
Andreas Mautsch <amautsch>
2023-01-06 11:50:02 +0100

    changes

--- src/main/java/org/goafabric/eventdispatcher/producer/EventData.java
+++ src/main/java/org/goafabric/eventdispatcher/producer/EventData.java
@@ -1,16 +1,14 @@
 package org.goafabric.eventdispatcher.producer;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonFormat
 public class EventData {
     private String referenceId;
 }
