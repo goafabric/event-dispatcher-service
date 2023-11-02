9ecc773adfae4c3e798a385429911938071e96d1
Andreas Mautsch <amautsch>
2023-11-02 07:57:50 +0100

    changes

--- src/main/java/org/goafabric/eventdispatcher/Application.java
+++ src/main/java/org/goafabric/eventdispatcher/Application.java
@@ -1,5 +1,6 @@
 package org.goafabric.eventdispatcher;
 
+import io.micrometer.observation.ObservationPredicate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
 
 
 /**
@@ -24,5 +26,8 @@ public class Application {
     public CommandLineRunner init(ApplicationContext context) {
         return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
     }
+
+    @Bean
+    ObservationPredicate disableHttpServerObservationsFromName() { return (name, context) -> !name.startsWith("spring.security.") && context instanceof ServerRequestObservationContext && !((ServerRequestObservationContext) context).getCarrier().getRequestURI().startsWith("/actuator"); }
 
 }
