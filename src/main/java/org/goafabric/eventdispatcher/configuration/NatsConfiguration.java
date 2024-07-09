50383b6fbef1346e4d20af6cf3ddd725bbb5fc45
Andreas Mautsch <amautsch>
2024-07-09 16:08:37 +0200

    changes

--- src/main/java/org/goafabric/eventdispatcher/configuration/NatsConfiguration.java
+++ src/main/java/org/goafabric/eventdispatcher/configuration/NatsConfiguration.java
@@ -43,8 +43,7 @@ public class NatsConfiguration {
         //only for testing one stream for all, for production it could be one stream per subject
         var streamConfig = StreamConfiguration.builder()
                 .name("all")
-                .subjects("*")
-                //.subjects("patient", "practitioner") //"patient.*", "practitioner.*")
+                .subjects("*") //"patient.*", "practitioner.*")
                 .storageType(StorageType.File)
                 .build();
 
