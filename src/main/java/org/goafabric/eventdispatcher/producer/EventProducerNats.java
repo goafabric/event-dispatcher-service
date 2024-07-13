0ad4d95a6ca129fb4ca5c328d0f82c50252192bb
Andreas Mautsch <amautsch>
2024-07-13 09:50:49 +0200

    changes

--- src/main/java/org/goafabric/eventdispatcher/producer/EventProducerNats.java
+++ src/main/java/org/goafabric/eventdispatcher/producer/EventProducerNats.java
@@ -2,7 +2,6 @@ package org.goafabric.eventdispatcher.producer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
 import io.nats.client.Connection;
@@ -21,7 +20,7 @@ public class EventProducerNats implements EventProducer {
     public EventProducerNats(Connection natsConnection, ObservationRegistry observationRegistry) {
         this.natsConnection = natsConnection;
         this.observationRegistry = observationRegistry;
-        this.objectMapper = new ObjectMapper(new CBORFactory());
+        this.objectMapper = new ObjectMapper();
     }
 
     public void produce(ChangeEvent changeEvent) {
