03682407e4647147b07296ff6ef9bf0228e8ecf8
Andreas Mautsch <amautsch>
2025-07-31 09:38:33 +0200

    websocket

--- src/main/java/org/goafabric/eventdispatcher/service/extensions/KafkaHealthIndicator.java
+++ src/main/java/org/goafabric/eventdispatcher/service/extensions/KafkaHealthIndicator.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-public class KafkaAdminHealthIndicator implements HealthIndicator {
+public class KafkaHealthIndicator implements HealthIndicator {
 
     private final KafkaAdmin kafkaAdmin;
 
-    public KafkaAdminHealthIndicator(KafkaAdmin kafkaAdmin) {
+    public KafkaHealthIndicator(KafkaAdmin kafkaAdmin) {
         this.kafkaAdmin = kafkaAdmin;
     }
 
@@ -20,6 +20,7 @@ public class KafkaAdminHealthIndicator implements HealthIndicator {
     public Health health() {
         try (AdminClient client = AdminClient.create(kafkaAdmin.getConfigurationProperties())) {
             client.describeCluster().clusterId().get(); // needs proper timeouts configured in application.yaml / configmap to not hang forever
+            //kafkaTemplate.send("health-check", "ping").get(); //would not do that, because it will send msgs in health check and topic also needs to exist
             return Health.up().withDetail("kafka", "Available").build();
         } catch (Exception e) {
             return Health.down(e).withDetail("kafka", "Unavailable").build();
