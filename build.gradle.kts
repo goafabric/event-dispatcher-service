d1f2cfd33c9ad70d5197fd34b2e33e2ab324cc8b
Andreas Mautsch <amautsch>
2025-08-03 10:23:47 +0200

    websocket

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ plugins {
 	id("org.sonarqube") version "6.2.0.5505"
 	id("org.cyclonedx.bom") version "2.3.1"
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
+
+	id("org.openrewrite.rewrite") version "7.12.1"
 }
 
 repositories {
@@ -101,3 +103,5 @@ openApi {
 	customBootRun { args.set(listOf("--server.port=8080")) }
 	tasks.forkedSpringBootRun { dependsOn("compileAotJava", "processAotResources") }
 }
+
+rewrite { activeRecipe("UpgradeSpringBoot_4_0", "UpgradeSpringBatch_6_0") }
