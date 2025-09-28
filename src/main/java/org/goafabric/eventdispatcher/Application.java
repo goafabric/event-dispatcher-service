30c53c917db9aadfcd200b6796bbf2d3d1d2429d
Andreas Mautsch <amautsch>
2025-09-28 08:56:16 +0200

    chang

--- src/main/java/org/goafabric/eventdispatcher/Application.java
+++ src/main/java/org/goafabric/eventdispatcher/Application.java
@@ -1,9 +1,7 @@
 package org.goafabric.eventdispatcher;
 
-import org.goafabric.event.EventData;
 import org.goafabric.eventdispatcher.service.controller.dto.Patient;
 import org.goafabric.eventdispatcher.service.controller.dto.Practitioner;
-import org.goafabric.eventdispatcher.service.controller.dto.SocketMessage;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.annotation.RegisterReflection;
 import org.springframework.boot.CommandLineRunner;
@@ -18,7 +16,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RegisterReflection(classes = {java.security.AccessController.class, javax.security.auth.Subject.class,
-        EventData.class, SocketMessage.class, Patient.class, Practitioner.class}
+        Patient.class, Practitioner.class}
         , memberCategories = {MemberCategory.DECLARED_CLASSES, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS}) @SuppressWarnings("java:S5738") //fix for ClassNotFoundexcpetion for both classes
 public class Application {
 
--- src/main/java/org/goafabric/eventdispatcher/service/controller/dto/SocketMessage.java
+++ src/main/java/org/goafabric/eventdispatcher/service/controller/dto/SocketMessage.java
@@ -1,5 +1,8 @@
 package org.goafabric.eventdispatcher.service.controller.dto;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+
+@RegisterReflectionForBinding(SocketMessage.class)
 public record SocketMessage (
     String message
 ) {}
