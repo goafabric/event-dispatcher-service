55417980c20265cafd21e030b270f3fd6b2ae7cb
Andreas Mautsch <amautsch>
2025-11-22 10:34:56 +0100

    spring boot upgrade

--- src/main/java/org/goafabric/eventdispatcher/Application.java
+++ src/main/java/org/goafabric/eventdispatcher/Application.java
@@ -1,10 +1,7 @@
 package org.goafabric.eventdispatcher;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Created by amautsch on 26.06.2015.
@@ -15,11 +12,6 @@ public class Application {
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner init(ApplicationContext context) {
-        return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
     }
 
 }
