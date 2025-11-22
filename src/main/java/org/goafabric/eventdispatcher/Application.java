d24ee4558433b38831961e363b11cfbba6a0ab29
Andreas Mautsch <amautsch>
2025-11-22 09:45:37 +0100

    spring boot upgrade

--- src/main/java/org/goafabric/eventdispatcher/Application.java
+++ src/main/java/org/goafabric/eventdispatcher/Application.java
@@ -1,7 +1,5 @@
 package org.goafabric.eventdispatcher;
 
-import org.goafabric.eventdispatcher.service.controller.dto.Patient;
-import org.goafabric.eventdispatcher.service.controller.dto.Practitioner;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.annotation.RegisterReflection;
 import org.springframework.boot.CommandLineRunner;
@@ -17,8 +15,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @RegisterReflection(classes = {
         java.security.AccessController.class, javax.security.auth.Subject.class,
-        org.apache.kafka.common.security.oauthbearer.DefaultJwtRetriever.class, org.apache.kafka.common.security.oauthbearer.DefaultJwtValidator.class,
-        Patient.class, Practitioner.class}
+        org.apache.kafka.common.security.oauthbearer.DefaultJwtRetriever.class, org.apache.kafka.common.security.oauthbearer.DefaultJwtValidator.class}
         , memberCategories = { MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS}) @SuppressWarnings("java:S5738") //fix for ClassNotFoundexcpetion for both classes
 public class Application {
 
--- src/main/java/org/goafabric/eventdispatcher/producer/EventProducer.java
+++ src/main/java/org/goafabric/eventdispatcher/producer/EventProducer.java
@@ -1,10 +1,16 @@
 package org.goafabric.eventdispatcher.producer;
 
 import org.goafabric.event.EventData;
+import org.goafabric.eventdispatcher.service.controller.dto.Patient;
+import org.goafabric.eventdispatcher.service.controller.dto.Practitioner;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.annotation.RegisterReflection;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@RegisterReflection(classes = {Patient.class, Practitioner.class}
+        , memberCategories = { MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS})
 public class EventProducer {
     private final KafkaTemplate<String, EventData> kafkaTemplate;
 
