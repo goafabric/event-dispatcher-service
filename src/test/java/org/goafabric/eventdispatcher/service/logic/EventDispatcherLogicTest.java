3e26a2422dca62b6136d86e88746e5334dac4c93
Andreas Mautsch <amautsch>
2026-01-13 16:39:11 +0100

    header handling

--- src/test/java/org/goafabric/eventdispatcher/service/logic/EventDispatcherLogicTest.java
+++ src/test/java/org/goafabric/eventdispatcher/service/logic/EventDispatcherLogicTest.java
@@ -1,8 +1,13 @@
 package org.goafabric.eventdispatcher.service.logic;
 
 import org.goafabric.eventdispatcher.producer.EventProducer;
+import org.goafabric.eventdispatcher.service.controller.dto.DbOperation;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class EventDispatcherLogicTest {
 
@@ -26,7 +31,10 @@ class EventDispatcherLogicTest {
         eventProducerLogic.createPractitioner();
         eventProducerLogic.updatePractitioner();
 
-        //Mockito.verify(eventProducer, Mockito.times(2)).produce(anyString(), anyString(), any(DbOperation.class), any(Patient.class));
+        verify(eventProducer, times(2))
+                .produce(anyString(), anyString(), eq(DbOperation.CREATE), any(Object.class));
+        verify(eventProducer, times(2))
+                .produce(anyString(), anyString(), eq(DbOperation.UPDATE), any(Object.class));
     }
 
 
